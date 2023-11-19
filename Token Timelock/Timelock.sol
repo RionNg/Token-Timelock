8b2366de83bbdc79c8c3d2f0b2c7e2db0d902c64
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-19 22:22:43 +0800

    Add files via upload

---
+++ Token Timelock/Timelock.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+contract Timelock {
+    // who can withdraw?
+    // when? 
+    // how much?
+    // https://www.unixtimestamp.com/
+
+    address payable public beneficiary;
+    uint256 public releaseTime;
+
+    constructor(
+        address payable _beneficiary,
+        uint256 _releaseTime
+    )
+        payable
+    {
+        require(_releaseTime > block.timestamp);
+        beneficiary = _beneficiary;
+        releaseTime = _releaseTime;
+    }
+
+    function release() public {
+        require(block.timestamp >= releaseTime);
+        (beneficiary).transfer(address(this).balance);
+    }
+
+}
